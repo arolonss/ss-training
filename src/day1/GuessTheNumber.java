8536f23cac28c77b228e29f093a3f6866c31fa2a
Amanda Rolon <amanda.rolon@smoothstack.com>
2021-05-20 22:25:57 -0400

    started 2nd assignment

---
+++ src/day1/GuessTheNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class GuessTheNumber {
+  static Scanner sc = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    greetUser();
+  }
+
+  public static void greetUser() {
+    System.out.println("Welcome to Guess the Number!");
+  }
+}
