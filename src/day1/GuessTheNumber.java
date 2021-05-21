55c21718e6175dcf6535af463a2c05f3190408ef
Amanda Rolon <amanda.rolon@smoothstack.com>
2021-05-21 11:31:05 -0400

    guess ranges working

--- src/day1/GuessTheNumber.java
+++ src/day1/GuessTheNumber.java
@@ -3,35 +3,57 @@ import java.util.Random;
 
 public class GuessTheNumber {
   static Scanner sc = new Scanner(System.in);
+  static Random rand = new Random();
+  static int secretNum = rand.nextInt(100);
+  static int guessLimit = 5;
 
   public static void main(String[] args) {
+    System.out.println(secretNum);
     greetUser();
   }
 
   public static void greetUser() {
     System.out.println("Welcome to Guess the Number!");
-    Random rand = new Random();
-    int secretNum = rand.nextInt(100);
-    int minusTen = secretNum - 10;
-    int plusTen = secretNum + 10;
+    getUserGuess();
+  }
+
+  public static void getUserGuess() {
+
     // String output = String.format("Pick a number from %d - %d", minusTen,
     // plusTen);
-    System.out.println("Pick a number from 1 - 100");
-    int userGuess = sc.nextInt();
-    System.out.println(userGuess);
-    System.out.println(secretNum);
-    System.out.println(minusTen);
-    System.out.println(plusTen);
-    // Range secretNumRange = new Range(minusTen, plusTen);
-    int guessLimit = 0;
-    while (userGuess != secretNum && guessLimit < 5) {
-      if (userGuess >= plusTen && userGuess <= minusTen) {
-        String closeGuessMsg = String.format("The correct answer is %d", secretNum);
-        System.out.println(closeGuessMsg);
-      } else {
-        System.out.println(secretNum);
-      }
-      guessLimit++;
+    // User is asked to guess a number 1-100. (Program chooses randomly.)
+    while (guessLimit > 0) {
+      System.out.println("Pick a number from 1 - 100");
+      int userGuess = sc.nextInt();
+      checkGuess(userGuess);
+      guessLimit--;
     }
+  }
+
+  public static void checkGuess(int userGuess) {
+
+    final int minusTen = secretNum - 10;
+    final int plusTen = secretNum + 10;
+
+    // getUserGuess();
+    // If the guess is within 10 of the correct answer, plus or minus, output the
+    // correct answer and exit.
+    if (userGuess >= minusTen && userGuess <= plusTen) {
+      String closeGuessMsg = String.format("The correct answer is %d", secretNum);
+      System.out.println(closeGuessMsg);
+      System.exit(0);
+      // If the number is not within 10 of the correct answer, ask the user to keep
+      // guessing.
+    } else if (userGuess <= minusTen && userGuess >= plusTen) {
+
+      System.out.println("tryagain");
+      getUserGuess();
+
+    } else if (guessLimit == 0) {
+      String gameOver = String.format("Sorry, the correct answer is %d", secretNum);
+      System.out.println(gameOver);
+      System.exit(0);
+    }
+
   }
 }
