d5d7d70fdb8eafad3fbb1b4fe3cc16c1b24926a2
Amanda Rolon <amanda.rolon@smoothstack.com>
2021-05-21 12:16:02 -0400

    requirements met

--- src/day1/GuessTheNumber.java
+++ src/day1/GuessTheNumber.java
@@ -8,51 +8,48 @@ public class GuessTheNumber {
   static int guessLimit = 5;
 
   public static void main(String[] args) {
-    System.out.println(secretNum);
     greetUser();
   }
 
   public static void greetUser() {
+    // greets user and starts game by calling gameLoop method
     System.out.println("Welcome to Guess the Number!");
-    getUserGuess();
+    gameLoop();
   }
 
-  public static void getUserGuess() {
-
-    // String output = String.format("Pick a number from %d - %d", minusTen,
-    // plusTen);
+  public static void gameLoop() {
     // User is asked to guess a number 1-100. (Program chooses randomly.)
     while (guessLimit > 0) {
       System.out.println("Pick a number from 1 - 100");
       int userGuess = sc.nextInt();
       checkGuess(userGuess);
+      System.out.println("Try again.");
       guessLimit--;
     }
+    String gameOver = String.format("Sorry, the correct answer is %d", secretNum);
+    System.out.println(gameOver);
+    System.exit(0);
   }
 
   public static void checkGuess(int userGuess) {
 
-    final int minusTen = secretNum - 10;
-    final int plusTen = secretNum + 10;
+    int tenLess = secretNum - 10;
+    int tenMore = secretNum + 10;
+    /*
+     * If the guess is within 10 of the correct answer, plus or minus, output the
+     * correct answer and exit.
+     */
 
-    // getUserGuess();
-    // If the guess is within 10 of the correct answer, plus or minus, output the
-    // correct answer and exit.
-    if (userGuess >= minusTen && userGuess <= plusTen) {
+    if (userGuess >= tenLess && userGuess <= tenMore) {
       String closeGuessMsg = String.format("The correct answer is %d", secretNum);
       System.out.println(closeGuessMsg);
       System.exit(0);
-      // If the number is not within 10 of the correct answer, ask the user to keep
-      // guessing.
-    } else if (userGuess <= minusTen && userGuess >= plusTen) {
-
-      System.out.println("tryagain");
-      getUserGuess();
-
-    } else if (guessLimit == 0) {
-      String gameOver = String.format("Sorry, the correct answer is %d", secretNum);
-      System.out.println(gameOver);
-      System.exit(0);
+      /*
+       * If the number is not within 10 of the correct answer, ask the user to keep
+       * guessing.
+       */
+    } else if (userGuess <= tenLess && userGuess >= tenMore) {
+      gameLoop();
     }
 
   }
