a415ac3159628d6f6e26adc8e15a7c7ffa23b93c
Amanda Rolon <abrolon87@gmail.com>
2021-05-22 11:19:11 -0400

    Triangle almost done; trying to figure out calculation formula

--- src/day1/com/ss/jb/two/Triangle.java
+++ src/day1/com/ss/jb/two/Triangle.java
@@ -2,16 +2,33 @@ package com.ss.jb.two;
 
 public class Triangle implements Shape {
 
+  int sideOne;
+  int sideTwo;
+  int sideThree;
+
+  public Triangle(int x, int y, int z) {
+    sideOne = (int) x;
+    sideTwo = (int) y;
+    sideThree = (int) z;
+  }
+
+  public static void main(String[] args) {
+    Triangle triangle = new Triangle(7, 2, 9);
+    triangle.display();
+  }
+
   @Override
-  public Integer calculateArea() {
-    // TODO Auto-generated method stub
-    return null;
+  public int calculateArea() {
+    // add all sides then devide by 2 to get semi-perimeter
+    int sp = (sideOne + sideTwo + sideThree) / 2;
+    //
+    int area = (int) Math.pow((sp * (sp - sideOne) * (sp - sideTwo) * (sp - sideThree)), 0.5);
+    return area;
   }
 
   @Override
   public void display() {
-    // TODO Auto-generated method stub
-
+    System.out.println(String.format("Triangle area: %d", this.calculateArea()));
   }
 
 }
