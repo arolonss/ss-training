0b041a9748a67f173d1632629583c00fe6edce15
Amanda Rolon <abrolon87@gmail.com>
2021-05-22 17:29:34 -0400

    Rectangle done

--- src/day1/com/ss/jb/two/Rectangle.java
+++ src/day1/com/ss/jb/two/Rectangle.java
@@ -1,17 +1,27 @@
 package com.ss.jb.two;
 
 public class Rectangle implements Shape {
+  int height;
+  int width;
+
+  public Rectangle(int h, int w) {
+    height = (int) h;
+    width = (int) w;
+  }
+
+  public static void main(String[] args) {
+    Rectangle rectangle = new Rectangle(5, 10);
+    rectangle.display();
+  }
 
   @Override
-  public Integer calculateArea() {
-    // TODO Auto-generated method stub
-    return null;
+  public int calculateArea() {
+    return this.height * this.width;
   }
 
   @Override
   public void display() {
-    // TODO Auto-generated method stub
-
+    System.out.println(String.format("Rectangle area: %d", this.calculateArea()));
   }
 
 }
