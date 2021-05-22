319a02e2a048f0c36dfcd633ecc7334e5165fb35
Amanda Rolon <abrolon87@gmail.com>
2021-05-22 11:16:41 -0400

    Circle done

--- src/day1/com/ss/jb/two/Circle.java
+++ src/day1/com/ss/jb/two/Circle.java
@@ -1,17 +1,28 @@
 package com.ss.jb.two;
 
+import java.lang.Math;
+
 public class Circle implements Shape {
 
+  int radius;
+
+  public Circle(int rad) {
+    radius = (int) rad;
+  }
+
+  public static void main(String[] args) {
+    Circle circle = new Circle(4);
+    circle.display();
+  }
+
   @Override
-  public Integer calculateArea() {
-    // TODO Auto-generated method stub
-    return null;
+  public int calculateArea() {
+    return (int) (Math.PI * Math.pow(this.radius, 2));
   }
 
   @Override
   public void display() {
-    // TODO Auto-generated method stub
-
+    System.out.println(String.format("Circle area: %d", this.calculateArea()));
   }
 
 }
