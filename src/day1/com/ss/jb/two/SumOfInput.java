e0214d349875d9a16240c013d5c21eef240a68de
Amanda Rolon <amanda.rolon@smoothstack.com>
2021-05-24 20:23:18 -0400

    closes scanner; deletes unused code

--- src/day1/com/ss/jb/two/SumOfInput.java
+++ src/day1/com/ss/jb/two/SumOfInput.java
@@ -23,29 +23,21 @@ public class SumOfInput {
     int counter = 0;
     int i;
 
-    // 3throws ArrayIndexOutOfBoundsException
-
     try {
       while (counter < 3) {
         i = sc.nextInt();
         addends[counter] = i;
         counter++;
       }
-
-      // System.out.println(addendOne + " + ...");
+      sc.close();
+      calcSum(addends);
 
     } catch (InputMismatchException e) {
       System.out.println("All three numbers must be integers! Please try again later.");
       System.exit(0);
-    } finally {
-      calcSum(addends);
     }
 
   }
-
-  // public static void checkInput() {
-
-  // }
 
   public static void calcSum(int[] arr) {
     int sum = 0;
@@ -53,23 +45,7 @@ public class SumOfInput {
       sum += arr[i];
     }
     System.out.println("The sum is " + sum);
+
     System.exit(0);
   }
 }
-
-// throws InputMismatchException
-// {
-// addendOne = sc.nextInt();
-
-// System.out.println(addendOne + " + ...");
-
-// // addendTwo = sc.nextInt();
-// // System.out.println(addendOne + " + " + addendTwo + " + ...");
-// // addendThree = sc.nextInt();
-// // System.out.println(addendOne + " + " + addendTwo + " + " + addendThree +
-// "...");
-// } catch (InputMismatchException e) {
-// System.out.println("All three numbers must be integers! Please try again.");
-// } finally {
-// calcSum(addendOne, addendTwo, addendThree);
-// }
